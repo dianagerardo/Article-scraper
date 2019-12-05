7fcebf591c5c77428524dd6aa54ebe9fc9635c42
Diana Gerardo <dianagerardo@email.arizona.edu>
2019-12-05 13:15:17 -0700

    adding cheerio script to scrape npr news website

--- server.js
+++ server.js
@@ -27,11 +27,63 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/articleCommentGenerator_DB", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/articleGenerator_DB", { useNewUrlParser: true });
+
+// Each scraped article should be saved to your application database. At a minimum, the app should scrape and display the following information for each article:
+
+//      * Headline - the title of the article
+
+//      * Summary - a short summary of the article
+
+//      * URL - the url to the original article
+
+//      * Feel free to add more content to your database (photos, bylines, and so on).
+
+app.get("/scrape", function (req, res) {
+    // First, we grab the body of the html with axios
+    axios.get("https://www.npr.org/sections/news/").then(function (response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        console.log(response.data)
+        var $ = cheerio.load(response.data);
+
+        // // Now, we grab every h2 within an article tag, and do the following:
+        $("article h2").each(function (i, element) {
+
+            //   // Save an empty result object
+            var result = {};
+
+            //   // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+            // result.image = $(this)
+            //     .children("img")
+            //     .attr("src");
+
+            //   // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    //       // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    //       // If an error occurred, log it
+                    console.log(err);
+                });
+        });
+
+
+        // Send a message to the client
+        res.send("Scrape Complete");
+    });
+});
 
 
 
 // Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
 });
