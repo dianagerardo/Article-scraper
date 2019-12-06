bf6c9b4353396abd393d188a69f372420931871d
Diana Gerardo <dianagerardo@email.arizona.edu>
2019-12-05 22:21:05 -0700

    Adding line to deploy to heroku

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/articleGenerator_DB", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articleGenerator_DB"
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Each scraped article should be saved to your application database. At a minimum, the app should scrape and display the following information for each article:
 
