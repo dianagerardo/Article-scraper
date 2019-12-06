9ce1311bda704d9fad5607ed46aeb70b5874ebbc
Diana Gerardo <dianagerardo@email.arizona.edu>
2019-12-05 22:44:59 -0700

    Fixing port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -151,6 +151,6 @@ app.get("/scrape", function (req, res) {
 
 
 // Start the server
-app.listen(PORT, function () {
-    console.log("App running on port " + PORT + "!");
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
